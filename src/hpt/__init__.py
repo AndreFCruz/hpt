d33ac358810549a902cf02b1cdbcce0811e01886
AndreFCruz <andrecruz97@gmail.com>
2023-02-01 15:09:51 +0100

    minor changes, version bump

--- src/hpt/__init__.py
+++ src/hpt/__init__.py
@@ -1,1 +1,3 @@
-from .version import __version__, __version_info__
+from ._version import __version__, __version_info__
+from .tuner import ObjectiveFunction, OptunaTuner
+from .suggest import suggest_random_hyperparams
